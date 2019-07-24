b1d32d0b9e2f5ede6876e8a7c811cb745ff9681b
Magesh <tkmagesh77@gmail.com>
2019-07-24 15:57:39 +0800

    Handling post requests

--- 07-app-server/server.js
+++ 07-app-server/server.js
@@ -13,20 +13,33 @@ const server = http.createServer((req, res) => {
 	console.log(req.url);
 	const urlObj = url.parse(req.url);
 	
-	if (urlObj.pathname !== '/calculator'){
+	if (urlObj.pathname === '/calculator' && req.method === 'GET'){
+		const queryData = querystring.parse(urlObj.query),
+			op = queryData.op,
+			n1 = parseInt(queryData.n1),
+			n2 = parseInt(queryData.n2),
+			result = calculator[op](n1, n2);
+
+		res.write(result.toString());
+		res.end();
+	} else if (urlObj.pathname === '/calculator' && req.method === 'POST'){
+		let rawData = '';
+		req.on('data', chunk => rawData += chunk);
+		req.on('end', () => {
+			const bodyData = querystring.parse(rawData),
+				op = bodyData.op,
+				n1 = parseInt(bodyData.n1),
+				n2 = parseInt(bodyData.n2),
+				result = calculator[op](n1, n2);
+
+			res.write(result.toString());
+			res.end();
+		})
+	} else {
 		res.statusCode = 404;
 		res.end();
 		return;
 	}
-
-	const queryData = querystring.parse(urlObj.query),
-		op = queryData.op,
-		n1 = parseInt(queryData.n1),
-		n2 = parseInt(queryData.n2),
-		result = calculator[op](n1, n2);
-
-	res.write(result.toString());
-	res.end();
 
 });
 
