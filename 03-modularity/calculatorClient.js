5614b438eb82ac014602522bb4aef38c50278197
Magesh <tkmagesh77@gmail.com>
2019-07-24 11:36:31 +0800

    Day-01 complete

---
+++ 03-modularity/calculatorClient.js
@@ -0,0 +1,12 @@
+const calculator = require('./calculator');
+
+console.log(calculator);
+
+const x = 100,
+	y = 50;
+
+
+console.log(calculator.add(x,y));
+console.log(calculator.subtract(x,y));
+console.log(calculator.multiply(x,y));
+console.log(calculator.divide(x,y));
