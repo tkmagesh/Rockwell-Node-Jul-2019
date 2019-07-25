14893cfe3072f36b0fe649259ad084d64e9ba78c
Magesh <tkmagesh77@gmail.com>
2019-07-25 15:33:58 +0800

    using promises

--- 10-async-demos/async.js
+++ 10-async-demos/async.js
@@ -95,3 +95,52 @@ function runAsync(){
 
 module.exports['runAsync'] = runAsync;
 
+function addSync(x,y){
+	console.log(`	[@Service] processing ${x} and ${y}`);
+	var result = x + y;
+	console.log(`	[@Service] return the result`);
+	return result;
+}
+
+function addSyncClient(x,y){
+	console.log(`[@Client] triggering the service`);
+	var result = addSync(x,y);
+	console.log(`[@Client] result = ${result}`);
+}
+
+module.exports['addSyncClient'] = addSyncClient;
+
+function addAsync(x,y, callback){
+	console.log(`	[@Service] processing ${x} and ${y}`);
+	setTimeout(function(){
+		var result = x + y;
+		console.log(`	[@Service] return the result`);
+		callback(result);
+	},4000)
+}
+
+function addAsyncClient(x,y){
+	console.log(`[@Client] triggering the service`);
+	addAsync(x,y, function(result){
+		console.log(`[@Client] result = ${result}`);
+	});
+}
+
+module.exports['addAsyncClient'] = addAsyncClient;
+
+
+function addAsyncPromise(x,y){
+	console.log(`	[@Service] processing ${x} and ${y}`);
+	var p = new Promise(function(resolveFn, rejectFn){
+		setTimeout(function(){
+			var result = x + y;
+			console.log(`	[@Service] return the result`);
+			resolveFn(result);
+		},4000);
+	});
+	return p;
+}
+
+
+
+module.exports['addAsyncPromise'] = addAsyncPromise;
